7b42f4c6a8eabffeaec5c90d19dcc1390b06137e
loginsonu <loginsonu.sharma@gmail.com>
2026-07-13 11:43:06 +0530

    Initial Kotlin Multiplatform project setup

---
+++ shared/src/commonMain/kotlin/com/sony/productbrowser/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.sony.productbrowser
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/sony/productbrowser/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.sony.productbrowser
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
